d7852291afb64c2ffa396822040bda88c9d1be0d
aspx <aspx>
2022-02-15 00:05:10 +0000

    test

--- deploy-px-k8s/background.sh
+++ deploy-px-k8s/background.sh
@@ -3,6 +3,10 @@ curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
 
 ssh root@[[HOST_IP]] 'echo "Host *" >> /root/.ssh/config && echo "    StrictHostKeyChecking no" >> /root/.ssh/config && chmod 400 /root/.ssh/config'
 
+ssh root@node01 'curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -'
+ssh root@node02 'curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -'
+ssh root@node03 'curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -'
+
 while [ ! -f /root/.kube/config ]
   do
     sleep 1
@@ -10,4 +14,5 @@ while [ ! -f /root/.kube/config ]
 if [ -f /root/.kube/start ]; then
   /root/.kube/start
 fi
+
 
