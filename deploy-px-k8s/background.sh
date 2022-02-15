76a6935123667cd1b11b0e97b36b2e68c0113685
aspx <aspx>
2022-02-15 00:55:23 +0000

    test

--- deploy-px-k8s/background.sh
+++ deploy-px-k8s/background.sh
@@ -3,7 +3,7 @@
 # Adding google key
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 
-curl -s https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+#curl -s https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 
 ssh root@[[HOST_IP]] 'echo "Host *" >> /root/.ssh/config && echo "    StrictHostKeyChecking no" >> /root/.ssh/config && chmod 400 /root/.ssh/config'
 
