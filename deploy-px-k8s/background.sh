d1384ac4c8b481f1f3fb38f56100d5150dcd906d
aspx <aspx>
2022-02-15 00:09:49 +0000

    test

--- deploy-px-k8s/background.sh
+++ deploy-px-k8s/background.sh
@@ -1,11 +1,12 @@
 # Adding google key
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 
 ssh root@[[HOST_IP]] 'echo "Host *" >> /root/.ssh/config && echo "    StrictHostKeyChecking no" >> /root/.ssh/config && chmod 400 /root/.ssh/config'
 
-ssh root@node01 'curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -'
-ssh root@node02 'curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -'
-ssh root@node03 'curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -'
+ssh root@node01 'curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -;curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -'
+ssh root@node02 'curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -;curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -'
+ssh root@node03 'curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -;curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -'
 
 while [ ! -f /root/.kube/config ]
   do
